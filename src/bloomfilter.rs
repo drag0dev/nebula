2c592217750f4c452e8fbdff74c33b3cae2b15c4
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-10-23 20:54:21 +0200

    reading and writing seeds

--- src/bloomfilter.rs
+++ src/bloomfilter.rs
@@ -3,6 +3,7 @@ use bitvec::prelude::BitVec;
 use murmur3::murmur3_x64_128;
 use anyhow::{Result, Context};
 use rand::Rng;
+use std::io::prelude::*;
 
 // TODO: a lot of unnecessary casting
 
@@ -25,7 +26,7 @@ pub struct BloomFilter{
 
 impl BloomFilter{
     /// function will panic if number of hash functions and number of seeds does not match
-    pub fn new(item_count: usize, fp_prob: f64, seeds: Option<Vec<u32>>) -> (Self, Option<Vec<u32>>){
+    pub fn new(item_count: usize, fp_prob: f64) -> (Self, Option<Vec<u32>>){
         // size = -(items * log(probability)) / (log(2)^2)
         let bit_arr_len = -((item_count as f64 * fp_prob.log(EULER_NUMBER)) /
                             (2_f64.log(EULER_NUMBER).powi(2) as f64))
@@ -35,7 +36,13 @@ impl BloomFilter{
         // hash functions = (size/item_count) * log(2)
         let hash_functions = ((bit_arr_len as f64 /item_count as f64) * 2_f64.log(EULER_NUMBER))
                             .round() as usize;
-        let seeds = match seeds{
+        let seeds = match load_seeds(){
+            Some(seeds) => {
+                if hash_functions != seeds.len() as usize{
+                    panic!("Number of hash functions has to match number of seeds!");
+                }
+                seeds
+            },
             None => {
                 // generate n seeds
                 let mut seeds: Vec<u32> = Vec::with_capacity(hash_functions);
@@ -43,12 +50,7 @@ impl BloomFilter{
                 for _ in 0..hash_functions{
                     seeds.push(rng.gen());
                 }
-                seeds
-            },
-            Some(seeds) => {
-                if hash_functions != seeds.len() as usize{
-                    panic!("Number of hash functions has to match number of seeds!");
-                }
+                write_seeds(&seeds);
                 seeds
             }
         };
@@ -115,6 +117,42 @@ fn closest_pow(n: usize) -> (usize, usize){
     (res, pow)
 }
 
+fn load_seeds() -> Option<Vec<u32>>{
+    let file_contents = std::fs::read_to_string("seeds.txt");
+    if file_contents.is_err(){
+        return None
+    }else{
+        let file_contents = file_contents.unwrap();
+        let file_contents = file_contents.trim();
+        let mut seeds: Vec<u32> = Vec::with_capacity(file_contents.matches("\n").count());
+
+        for seed in file_contents.split("\n"){
+            let seed = seed.parse::<u32>();
+            if seed.is_err(){
+                panic!("error: parsing a seed \"{:?}\"", seed.err());
+            }else{
+                seeds.push(seed.unwrap());
+            }
+        }
+        return Some(seeds);
+    }
+}
+
+fn write_seeds(seeds: &Vec<u32>){
+    let file = std::fs::File::create("seeds.txt");
+    if file.is_err(){
+        panic!("error: creating \"seeds.txt\" file");
+    }
+    let mut file = file.unwrap();
+    for s in seeds.iter(){
+        match file.write_all(format!("{}\n", s).as_bytes()){
+            Ok(_) => {},
+            Err(e) => {
+                panic!("error: writing a seed \"{}\"", e);
+            }
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests{
@@ -122,7 +160,7 @@ mod tests{
 
     #[test]
     fn present_value(){
-        let (mut bf, _) = BloomFilter::new(100_000, 0.02, None);
+        let (mut bf, seeds) = BloomFilter::new(100_000, 0.02);
 
         assert_eq!(bf.add("temp").unwrap(), ());
         assert_eq!(bf.check("temp").unwrap(), true);
@@ -130,5 +168,11 @@ mod tests{
         assert_eq!(bf.check("temp1").unwrap(), false);
         assert_eq!(bf.add("temp1").unwrap(), ());
         assert_eq!(bf.check("temp1").unwrap(), true);
+    }
+
+    #[test]
+    fn read_and_write_seeds(){
+        write_seeds(&vec![1, 2, 3]);
+        assert_eq!(load_seeds(), Some(vec![1, 2, 3]));
     }
 }
