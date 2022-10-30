0dc768cb132d5eefcc92fff379d2efdfb4cf0610
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-10-30 21:17:19 +0100

    start cli in main

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ mod utils;
 mod parser;
 
 fn main() {
+    let quit_status = parser::cli();
+    if let Some(err) = quit_status.err(){
+        println!("error: encountered while parsing command\n{:?}", err);
+    }
 }
