a66eafd01085398e792552ea6c08910950cc890e
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-10-30 21:17:19 +0100

    start cli in main

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,8 @@ mod hyperloglog;
 mod parser;
 
 fn main() {
+    let quit_status = parser::cli();
+    if let Some(err) = quit_status.err(){
+        println!("error: encountered while parsing command\n{:?}", err);
+    }
 }
