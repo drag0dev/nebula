3b66ea220f8e206098633766036db0436a3e7cc5
teodord25 <djuric.teodor25@gmail.com>
2022-11-13 22:24:21 +0100

    minor change to merge_sort tests

--- src/utils/merge_sort.rs
+++ src/utils/merge_sort.rs
@@ -7,7 +7,7 @@ fn merge<T: Copy + Ord>(left_half: &[T], right_half: &[T], y: &mut [T]) {
         if left_half[l] < right_half[r] {
             y[i] = left_half[l];
             l += 1;
-        }else{
+        } else {
             y[i] = right_half[r];
             r += 1;
         }
@@ -15,52 +15,56 @@ fn merge<T: Copy + Ord>(left_half: &[T], right_half: &[T], y: &mut [T]) {
     }
 
     // get all remaining items if any
-    if l < left_half.len(){
+    if l < left_half.len() {
         y[i..].copy_from_slice(&left_half[l..]);
     }
-    if r < right_half.len(){
-        y[i..].copy_from_slice(&right_half[r..]); }
+    if r < right_half.len() {
+        y[i..].copy_from_slice(&right_half[r..]);
+    }
 }
 
 pub fn merge_sort<T: Copy + Ord>(original_arr: &mut [T]) {
-        let len = original_arr.len();
-        if len <= 1{
-            return;
-        }
+    let len = original_arr.len();
+    if len <= 1 {
+        return;
+    }
 
-        let mid = len / 2;
+    let mid = len / 2;
 
-        merge_sort(&mut original_arr[..mid]);
-        merge_sort(&mut original_arr[mid..]);
+    merge_sort(&mut original_arr[..mid]);
+    merge_sort(&mut original_arr[mid..]);
 
-        let mut working_arr: Vec<T> = original_arr.to_vec();
+    let mut working_arr: Vec<T> = original_arr.to_vec();
 
-        merge(&original_arr[..mid], &original_arr[mid..], &mut working_arr[..]);
+    merge(
+        &original_arr[..mid],
+        &original_arr[mid..],
+        &mut working_arr[..],
+    );
 
-        original_arr.copy_from_slice(&working_arr);
+    original_arr.copy_from_slice(&working_arr);
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
+    use rand::random;
 
     #[test]
-    fn sorting(){
-        let mut test_vec = vec![5, 1 , 2, 4, 3];
-        merge_sort(&mut test_vec);
-        assert_eq!(test_vec, vec![1, 2, 3, 4, 5]);
+    fn sorting() {
+        let test_vec: Vec<i32> = (1..500_000).map(|_| random()).collect();
 
-        let mut test_vec = vec![5, 1 , 2, 4, 3];
-        merge_sort(&mut test_vec);
-        assert_eq!(test_vec, vec![1, 2, 3, 4, 5]);
+        let mut vec1 = test_vec.to_vec();
+        let mut vec2 = test_vec.to_vec();
 
-        let mut test_vec = vec![5, 1 , 2, 3, 4];
-        merge_sort(&mut test_vec);
-        assert_eq!(test_vec, vec![1, 2, 3, 4, 5]);
+        merge_sort(&mut vec1);
+        vec2.sort();
+
+        assert_eq!(vec1, vec2);
     }
 
     #[test]
-    fn empty(){
+    fn empty() {
         let mut test_vec: Vec<u64> = Vec::new();
         merge_sort(&mut test_vec);
         assert_eq!(test_vec.len(), 0);
