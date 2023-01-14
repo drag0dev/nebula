1585434773dbdbcf4df861a2140638b2c22b9e41
Dragoslav Tamindžija <dragodev@protonmail.com>
2023-01-14 17:08:36 +0100

    cli parser (#6)

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ mod bloomfilter;
 mod count_min_sketch;
 mod utils;
 mod hyperloglog;
+mod parser;
 
 fn main() {
+    let quit_status = parser::cli();
+    if let Some(err) = quit_status.err(){
+        println!("error: encountered while parsing command\n{:?}", err);
+    }
 }
