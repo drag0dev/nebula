1d56acea4cc9bfbd5dc4309691cd38e93f35d237
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-08-15 18:24:00 +0200

    empty out main

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,4 @@ mod building_blocks;
 mod repl;
 
 fn main() {
-    loop {
-        let q = repl::repl().unwrap();
-        match q.commands {
-            repl::Commands::Quit => break,
-            _ => {},
-        }
-    }
 }
