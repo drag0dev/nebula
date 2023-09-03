746ada0d91732b65b8bb31063c4c1087b7f81548
Dragoslav Tamindzija <dragodev@protonmail.com>
2023-09-03 09:40:00 +0200

    impl clear and init handlers

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,30 @@ mod repl;
 mod test_data_gen;
 mod utils;
 
+use std::{path::Path, fs::{create_dir, remove_file, remove_dir_all}};
 use crate::engine::Engine;
-use anyhow::{Context, Error, Result};
+use anyhow::{Context, Error, Result, anyhow};
 use clap::Parser;
 use cli::CliCommands;
 use test_data_gen::generate_test_data;
 
-fn main() -> Result<()> {
+fn main() {
     let args = cli::CliCommands::parse();
     match args {
-        CliCommands::Initialize => todo!(),
-        CliCommands::Delete => todo!(),
+        CliCommands::Init => {
+            if let Err(e) = initialize_fs() {
+                print_err(e);
+            } else {
+                println!("successfully initialized");
+            }
+        }
+        CliCommands::Clear => {
+            if let Err(e) = clear_fs() {
+                print_err(e);
+            } else {
+                println!("successfully cleared");
+            }
+        }
         CliCommands::Start => {
             let engine = Engine::new().context("oh goober!");
             if let Err(e) = engine {
@@ -28,7 +41,6 @@ fn main() -> Result<()> {
                 }
             }
 
-            Ok(())
         }
         CliCommands::GenerateTestData => {
             if let Err(e) = generate_test_data() {
@@ -36,7 +48,6 @@ fn main() -> Result<()> {
             } else {
                 println!("successfully generated test data");
             }
-            Ok(())
         }
         CliCommands::DummyData { file_name } => todo!(),
     }
@@ -47,4 +58,59 @@ fn print_err(e: Error) {
     for (i, small_e) in e.chain().enumerate().skip(1) {
         println!("{}{small_e}", "\t".repeat(i));
     }
+}
+
+// creates all the directories and generates a default config
+// data -
+//      -> table_data
+//      -> WAL
+// TODO: generate default config
+fn initialize_fs() -> Result<()> {
+    // top "data" dir
+    let path = Path::new("./data");
+    if !path.is_dir() {
+        create_dir("./data").context("creating './data' dir")?;
+    }
+
+    // "table_data" dir
+    let path = Path::new("./data/table_data");
+    if !path.is_dir() {
+        create_dir("./data/table_data").context("creating './data/table_data' dir")?;
+    }
+
+    // "WAL" dir
+    let path = Path::new("./data/WAL");
+    if !path.is_dir() {
+        create_dir("./data/WAL").context("creating './data/WAL' dir")?;
+    }
+    Ok(())
+}
+
+fn clear_fs() -> Result<()> {
+    if Path::new("./data").is_dir() {
+        let path = Path::new("./data/table_data");
+        if path.is_dir() {
+            let entries = path.read_dir().context("reading 'table_data'")?;
+            for entry in entries {
+                let entry = entry.context("reading dir in 'table_data'")?;
+                remove_dir_all(entry.path()).context("removing file in 'table_data'")?;
+            }
+        } else {
+            return Err(anyhow!("missing dir './data/table_data'"));
+        }
+
+        let path = Path::new("./data/WAL");
+        if path.is_dir() {
+            let entries = path.read_dir().context("reading 'WAL'")?;
+            for entry in entries {
+                let entry = entry.context("reading file in 'WAL'")?;
+                remove_file(entry.path()).context("removing file in 'WAL'")?;
+            }
+        } else {
+            return Err(anyhow!("missing dir './data/WAL'"));
+        }
+    } else {
+        return Err(anyhow!("missing dir './data'"));
+    }
+    Ok(())
 }
